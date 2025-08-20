7263b364bbd3821dce602922fc6add3b9f9c2c7f
Salah-Essam <salah212essam@gmail.com>
2025-08-20 07:53:43 +0300

    adding firebase

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class InstaFood extends StatelessWidget {
   const InstaFood({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp();
