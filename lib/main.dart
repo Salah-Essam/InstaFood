b0bd41ee371ef44a07b0ef86776829e2bbb1a210
Salah-Essam <salah212essam@gmail.com>
2025-08-20 07:08:12 +0300

    1st

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const InstaFood());
+}
+
+class InstaFood extends StatelessWidget {
+  const InstaFood({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
