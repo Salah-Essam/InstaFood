7bc0d87b08db41aa7aa8c5b80d603638693c1d1b
Salah-Essam <salah212essam@gmail.com>
2025-08-31 22:25:21 +0300

    -

--- lib/presentation/widgets/app_backbutton.dart
+++ lib/presentation/widgets/app_backbutton.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:go_router/go_router.dart';
 import 'package:insta_food/core/theme/app_assets.dart';
 
 class AppBackButton extends StatelessWidget {
