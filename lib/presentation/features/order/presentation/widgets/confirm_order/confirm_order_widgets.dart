4adff8f3513d97abfdec541806e0e93bd7728308
RowanFayez <fayezrowan5@gmail.com>
2025-09-01 08:43:17 +0200

    refactor

---
+++ lib/presentation/features/order/presentation/widgets/confirm_order/confirm_order_widgets.dart
@@ -0,0 +1,6 @@
+export 'shipping_address_section.dart';
+export 'order_summary_header.dart';
+export 'light_dividers.dart';
+export 'order_summary_list.dart';
+export 'totals_section.dart';
+export 'place_order_button.dart';
